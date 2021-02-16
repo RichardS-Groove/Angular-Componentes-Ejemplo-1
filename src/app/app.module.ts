9f8359729fc23c4c1aa3fde31bd508a8421ac3a1
RichardS <richardsjcb@gmail.com>
2021-02-15 21:40:45 -0300

    Se sube el ejempo completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SumarComponent } from './components/sumar/sumar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SumarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sumar/sumar.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'sumar',
+  templateUrl: './sumar.component.html',
+  styleUrls: ['./sumar.component.css']
+})
+export class SumarComponent implements OnInit {
+
+  num1: number;
+  num2: number;
+  resultado: number;
+
+  constructor() {
+    this.num1 = 10;
+    this.num2 = 100;
+    this.resultado = null;
+
+  }
+
+  ngOnInit(): void {}
+
+
+  onclick() {
+    this.resultado = this.num1 + this.num2;
+  }
+
+}
